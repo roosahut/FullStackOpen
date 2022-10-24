21ff90d1f8b8fa464431b188d5140b24a380cd31
Roosa Huttunen <huttunenroosa00@gmail.com>
2022-10-24 15:10:47 +0300

    4.8-4.12 done

---
+++ osa4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('returns the correct amount of blogs as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('the identifier is called id', async () => {
+  const response = await api.get('/api/blogs')
+  response.body.forEach(blog => {
+    expect(blog.id).toBeDefined()
+  })
+})
+
+test('a blog can be added', async () => {
+  const newBlog = {
+    title: 'koirat',
+    author: 'kissa',
+    url: 'www.kissa.lol',
+    likes: 100
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(n => n.title)
+  expect(titles).toContain(
+    'koirat'
+  )
+})
+
+test('if likes is not given a value it is 0', async () => {
+  const newBlog = {
+    title: 'kissa',
+    author: 'jee',
+    url: 'hmm'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const likes = blogsAtEnd.map(n => n.likes)
+  expect(likes[helper.initialBlogs.length]).toBe(0)
+})
+
+test('blog without title and url is not added', async () => {
+  const newBlog = {
+    author: 'jee'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
