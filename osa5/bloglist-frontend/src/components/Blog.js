bbebf40257cb749563ce856d0a67f56e3352c693
Roosa Huttunen <huttunenroosa00@gmail.com>
2022-10-31 01:36:21 +0200

    5.7 done

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,44 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [state, changeState] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const changeShowStatus = () => {
+    if (state === true) {
+      changeState(false)
+    } else if (state === false) {
+      changeState(true)
+    }
+  }
+
+  const label = state
+    ? 'hide' : 'view'
+
+  const showRest = () => (
+    <div>
+      <p>{blog.url}</p>
+      <p>likes: {blog.likes}
+        <button>like</button>
+      </p>
+      <p>{blog.user.name}</p>
+    </div>
+  )
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={changeShowStatus}>{label}</button>
+      {state === true ? showRest() : null}
+    </div >
+  )
+}
 
 export default Blog
